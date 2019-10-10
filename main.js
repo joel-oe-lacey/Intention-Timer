f0bf7086def676662e13ed9f29925b906c41a4da
Joel Lacey <joellaceybusiness@gmail.com>
2019-10-10 15:04:37 -0600

    Add warning messages and validation for user goal and goal duration inputs

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+//Target inputs, warnings and start button
 var durationMinutesInput = document.querySelector("#duration-minutes");
 var durationMinutesWarning = document.querySelector("#minutes-warning");
 var durationSecondsInput = document.querySelector("#duration-seconds");
@@ -7,27 +8,35 @@ var goalInput = document.querySelector("#goal-input");
 var goalWarning = document.querySelector("#goal-warning");
 var startButton = document.querySelector("#start-button");
 
+// Define function to check if user inputs number
 function numberValidation(inputBox, warning) {
   // Define allowed character set within brackets, in this case only numbers
   // + checks that it matches the character set at least once, ^ and $ make sure the beginning and end match
   var regex = /^[0-9]+$/;
   var testValues = inputBox.value;
 
+  // Warnings start with hidden class, remove this if the user enters a non-number
   if (testValues.match(regex)===null) {
-    warning.classList.toggle('show');
+    warning.classList.remove('hide');
+    inputBox.value = "";
+  } else {
+    warning.classList.add('hide');
   }
 }
 
-function valueEntered(inputBox) {
+// Define a function to see if the user inputted anything into goal input
+function valueEntered(inputBox, warning) {
   var testValues = inputBox.value;
 
+  //Check if the user entered any characters to the goal input, if so show warning, else hide
   if(testValues.length === 0) {
-  console.log("hit case");
+    warning.classList.remove('hide');
   } else {
-  console.log("miss case");
+    warning.classList.add('hide');
   }
 }
 
+// Execute functions based on user input field interaction, or start button click 
 durationMinutesInput.addEventListener("change", function() {
     numberValidation(durationMinutesInput, durationMinutesWarning);
 });
@@ -37,5 +46,5 @@ durationSecondsInput.addEventListener("change", function() {
 });
 
 startButton.addEventListener("click", function() {
-    valueEntered(goalInput);
+    valueEntered(goalInput, goalWarning);
 });
